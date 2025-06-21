6934ac51be784ea9e7946a2b08f1c63b9f0260b8
KimV <71965590+dezineskv@users.noreply.github.com>
2025-06-21 00:10:56 -0700

    adjustment of board controller display

--- src/App.jsx
+++ src/App.jsx
@@ -69,30 +69,30 @@ function App() {
       </Routes>
 
       <div className="w-screen flex flex-col items-center justify-center">
-         <h2 className="w-full text-2xl font-bold text-black mt-10 mb-2 ml-2">
+        <h2 className="w-full text-2xl font-bold text-black mt-10 mb-2 ml-2">
           Color Power
         </h2>
-         <div className="w-full flex items-center justify-center mx-auto px-6 mb-5">
+        <div className="w-full flex items-center justify-center mx-auto px-6 mb-5">
           <div className="bg-black flex flex-col md:flex-row justify-center gap-2 rounded-xlg mx-auto p-10 max-w-3xl">
-            <div className="w-[200px] min-w-[160px] flex flex-col justify-center gap-4  bg-white rounded-lg items-center mx-auto my-2 pb-2">
-              <div className="w-[160px]">
+            <div className="w-9/12 min-w-[160px] flex flex-col justify-center gap-4  bg-white rounded-lg items-center mx-auto my-2 pb-2">
+              <div className="w-full mx-auto">
                 <h5 className="text-black text-xs font-bold pt-3 px-2">
                   BOARD CONTROLLER
                 </h5>
               </div>
 
-              <div className="flex flex-col items-center w-[160px] bg-white text-black px-2">
+              <div className="flex flex-row justify-center items-center gap-10 w-full mx-auto w-full bg-white text-black px-2">
                 <div
-                  className="items-center border-4 border-solid border-gray-600 rounded-full p-2 mb-2 size-16"
+                  className="w-[80px] h-[80px] items-center border-4 border-solid border-gray-600 rounded-full px-2 py-5 mb-2 size-16"
                   onClick={turnAllOff}
                 >
-                  <div className="text-xs  inline-flex ">Switch Off</div>
+                  <div className="h-full text-xs my-auto inline-flex">Switch Off</div>
                 </div>
                 <div
-                  className="items-center border-4 border-solid border-gray-600 rounded-full p-2 mb-2 size-16"
+                  className="w-[80px] h-[80px] items-center border-4 border-solid border-gray-600 rounded-full px-2 py-5 mb-2 size-16"
                   onClick={turnAllOn}
                 >
-                  <div className="text-xs  inline-flex ">Switch On</div>
+                  <div className="h-full text-xs my-auto inline-flex">Switch On</div>
                 </div>
               </div>
             </div>
@@ -101,7 +101,7 @@ function App() {
             </div>
           </div>
         </div>
-       {/* next section */}
+        {/* next section */}
         {/* <div className="w-full flex items-center justify-center mx-auto px-6 mb-5">
           <div className="bg-black flex flex-col md:flex-row justify-center gap-2 rounded-xlg mx-auto p-10 max-w-4xl">
             <div className="w-[200px] min-w-[160px] flex flex-col justify-center gap-4 bg-white rounded-lg items-center mx-auto my-2 pb-2 ml-2">
@@ -150,7 +150,6 @@ function App() {
             </div>
           </div>
         </div> */}
-     
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,18 +8,21 @@ function Home() {
     <>
       <Cards />
       <div className="flex justify-center mx-auto mt-10 max-w-screen-md">
-        <div className="rounded-lg border shadow-sm bg-white border-slate-200 shadow-slate-950/5 relative flex max-h-[30rem] w-full max-w-[50rem] flex-col items-end justify-center overflow-hidden text-center">
+        <div className="rounded-lg border shadow-sm bg-white border-slate-200 shadow-slate-950/5 relative flex max-h-[30rem] w-full max-w-[41rem] flex-col items-end justify-center overflow-hidden text-center">
           <div className="p-2">
             <div className="absolute inset-0 m-0 h-full w-full rounded-none bg-cover bg-center">
               <img
-                className="absolute top-0  left-0 w-full h-full opacity-90"
+                className="absolute top-0 left-0 w-full h-full opacity-90"
                 src="https://wallpaperbat.com/img/832177-full-stack-wallpaper.jpg"
               />
-              <div className="absolute inset-0 h-full w-full bg-gradient-to-t from-slate-800/80 via-black/50 to-slate-700/10 dark:from-black/90 dark:via-black/60 dark:to-black/20"></div>
+              {/* <div className="absolute inset-0 h-full w-full bg-gradient-to-t from-slate-800/80 via-black/50 to-slate-700/10 dark:from-black/90 dark:via-black/60 dark:to-black/20"></div> */}
             </div>
           </div>
           <div className="w-full rounded relative bottom-0 flex h-full flex-col items-center justify-startd px-6 py-14 md:px-12">
-            <h6 className="font-sans antialiased font-bold text-base md:text-lg lg:text-xl my-6" style={{color: 'red', fontWeight: 900, fontFamily: 'cursive'}}>
+            <h6
+              className="font-sans antialiased font-bold text-base md:text-lg lg:text-xl my-6"
+              style={{ color: "red", fontWeight: 900, fontFamily: "cursive" }}
+            >
               Kim Vidal
             </h6>
             <img
